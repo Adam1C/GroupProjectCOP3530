ca9807edc8bf2c5281322602ceda83d42ec09706
Adam <chaudhary.adam@ufl.edu>
2023-08-05 02:40:51 -0400

    updated fileinput to correctly grab input files 1-5 or specified input file

--- Main.cpp
+++ Main.cpp
@@ -1,58 +1,71 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <string>
-#include "Patients.h"
-#include "MaxHeap.h"
+#include <fstream>
+#include <sstream>
+#include "MaxHeap.h" 
+
+void heapToSortedArray(std::vector<int>& heap) {
+    std::sort_heap(heap.begin(), heap.end());
+}
 
 int main() {
-    MaxHeap triageQueue;
+    MaxHeap maxHeap; // Create an instance of MaxHeap
 
-    while (true) {
-        std::cout << "1. Add Patient\n";
-        std::cout << "2. Process Next Patient\n";
-        std::cout << "3. Exit\n";
-        std::cout << "Select an option: ";
+    // Open the input text file
+    std::string fileNumber; 
+    std::cout << "Enter the file number (1-5) or file name: ";
+    std::cin >> fileNumber;
+    if (fileNumber == "1")
+        std::ifstream inputFile("input1.txt");
+    else if (fileNumber == "2")
+        std::ifstream inputFile("input2.txt");
+    else if (fileNumber == "3")
+        std::ifstream inputFile("input3.txt");
+    else if (fileNumber == "4")
+        std::ifstream inputFile("input4.txt");
+    else if (fileNumber == "5")
+        std::ifstream inputFile("input5.txt");
+    else
+        std::ifstream inputFile(fileName);
 
-        int choice;
-        std::cin >> choice;
+    if (!inputFile.is_open()) {
+        std::cout << "Failed to open the input file." << std::endl;
+        return 1;
+    }
 
-        if (choice == 1) {
+    std::string line;
+    Patients patient("", 0); // Initialize a patient object
+
+    while (std::getline(inputFile, line)) {
+        std::istringstream iss(line);
+        std::string section;
+        iss >> section;
+
+        if (section == "PatientInfo:") {
             std::string name;
             int age;
-            std::cout << "Enter patient's name: ";
-            std::cin >> name;
-            std::cout << "Enter patient's age: ";
-            std::cin >> age;
-
-            Patients patient(name, age);
-
-            // Collect other patient information such as conditions, ailments, etc.
-            // Add them to the patient instance.
-
-            patient.calculateTriageValue();
-            triageQueue.push(patient);
-
-            std::cout << "Patient added to the queue.\n";
+            iss >> name >> age;
+            patient = Patients(name, age);
         }
-        else if (choice == 2) {
-            if (!triageQueue.empty()) {
-                Patients nextPatient = triageQueue.pop();
-                std::cout << "Processing next patient:\n";
-                std::cout << "Name: " << nextPatient.getName() << "\n";
-                std::cout << "Age: " << nextPatient.getAge() << "\n";
-                // Display other patient information.
-            }
-            else {
-                std::cout << "No patients in the queue.\n";
+        else if (section == "PriorConditions:") {
+            std::string condition;
+            while (iss >> condition) {
+                patient.addPriorCondition(condition);
             }
         }
-        else if (choice == 3) {
-            std::cout << "Exiting...\n";
-            break;
-        }
-        else {
-            std::cout << "Invalid choice. Please select a valid option.\n";
+        else if (section == "CurrentConditions:") {
+            std::string condition;
+            while (iss >> condition) {
+                patient.addCurrentCondition(condition);
+            }
+            maxHeap.insert(patient); // Insert the patient into the max heap
         }
     }
+    inputFile.close(); // Close the input file
+
+
+    // Now you have populated your max heap with patients and their conditions from the file
+    // Perform any operations you need with the max heap
 
     return 0;
 }
